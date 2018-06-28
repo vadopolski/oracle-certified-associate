8b64e1e68932adb5ef47e9dd57c027f8ae6ed2cc
Opolskiy <Opolskiy@bpcbt.com>
2018-06-28 11:51:13 +0300

    Added printing main class argument.

---
+++ ch1-java-building-blocks/src/main/java/Zoo.java
@@ -0,0 +1,6 @@
+public class Zoo {
+	public static void main(String[] args) {
+		System.out.println("First Arguments is: " + args[0]);
+		System.out.println("Second argument is: " + args[1]);
+	}
+}
