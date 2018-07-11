2ec38f9c41bc67af713afde07b95b754f5bee741
vadopolski <vop444@gmail.com>
2018-07-11 21:43:25 +0300

    Added java building block module

--- ch2-operators-and-statements/src/test/java/ReturnOfOperationTest.java
+++ ch2-operators-and-statements/src/test/java/ReturnOfOperationTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 public class ReturnOfOperationTest {
 
     @Test
-    public void intPlusLong() throws Exception {
+    public void intPlusLong_long                                                                                                    () throws Exception {
         int i = 5;
         long j = 5L;
 
@@ -13,21 +13,27 @@ public class ReturnOfOperationTest {
     }
 
     @Test
-    public void doublePlusFloat() throws Exception {
+    public void doublePlusFloat_double() throws Exception {
         double d = 5.25;
         float f = 5.7f;
 
-        assertTrue(Double.class.isInstance(d+f));
+        assertTrue(Double.class.isInstance(d + f));
     }
 
     @Test
-    public void shortDivideShort() throws Exception {
+    public void shortDivideShort_int() throws Exception {
         short sh1 = 6;
         short sh2 = 36;
-
 
         Integer.class.isInstance(sh1 + sh2);
     }
 
+    @Test
+    public void shortFloatDouble_double() throws Exception {
+        short x = 14;
+        float y = 13;
+        double z = 30;
 
+        Double.class.isInstance(x * y / z);
+    }
 }
